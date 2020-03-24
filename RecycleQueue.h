cc0811872bee71b36df6394f906adb907f600cf9
wuguangda <653322694@qq.com>
2020-03-24 20:03:03 +0800

    gf

---
+++ RecycleQueue.h
@@ -0,0 +1,20 @@
+#ifndef _RECYCLEQUEUE_H
+#define _RECYCLEQUEUE_H
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+template <typename stType>
+class RecycleQueue
+{
+	public:
+		stType **m_queue;
+		volatile uint64_t m_readPos;
+		volatile uint64_t m_writePos;
+		uint64_t m_unDropCount;
+		uint64_t m_rqSize;
+		uint64_t m_rqmask;
+};
+
+#endif 
